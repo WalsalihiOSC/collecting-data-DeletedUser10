c54de1307826d43e6db6124a4a8ae94838e52c2d
Ayush Kumar <90948894+DeletedUser10@users.noreply.github.com>
2022-04-10 14:49:52 +1200

    Created third frame
    When Show All button is pressed, frame 2 is hidden and frame 3 displays

--- Data collection task.py
+++ Data collection task.py
@@ -3,62 +3,90 @@ from tkinter import*
 class Interface:
     def __init__(self, parent):
         self.data = UI()
-        main_frame = Frame(parent)
-        main_frame.grid()
+        self.main_frame = Frame(parent)
+        self.main_frame.grid()
         
-        frame1 = Frame(main_frame, highlightbackground="black", 
+        self.frame1 = Frame(self.main_frame, highlightbackground="black", 
                                     highlightthickness=1, 
                                     height=60, 
                                     width=300, bg = "Pink")
 
-        frame1.grid(column=0, row = 0, padx=2, pady=2)
-        frame1.grid_propagate(0)
+        self.frame1.grid(column=0, row = 0, padx=2, pady=2)
+        self.frame1.grid_propagate(0)
         
-        data_label = Label(frame1, text = "Collecting Person Data", bg="pink")
-        data_label.grid(row=0, column=0, padx=10)
-        ghost_Label = Label(frame1, text = "               ", bg = "Pink").grid(column=2, row=0)
+        self.data_label = Label(self.frame1, text = "Collecting Person Data", bg="pink")
+        self.data_label.grid(row=0, column=0, padx=10)
+        ghost_Label = Label(self.frame1, text = "               ", bg = "Pink").grid(column=2, row=0)
 
-        button1 = Button(frame1, text = "Show All")
-        button1.grid(column=3, row=0, pady=10,sticky=NSEW)
+        self.button1 = Button(self.frame1, text = "Show All", command=self.frame_3)
+        self.button1.grid(column=3, row=0, pady=10,sticky=NSEW)
 
-        frame2 = Frame(main_frame, highlightbackground="black", highlightthickness=1, 
+        self.frame2 = Frame(self.main_frame, highlightbackground="black", highlightthickness=1, 
                                     height=200, 
                                     width=300, bg = "White")
-        frame2.grid(row=1, column=0)
-        frame2.grid_propagate(0)
+        self.frame2.grid(row=1, column=0)
+        self.frame2.grid_propagate(0)
         
-        l1 = Label(frame2, text = "First name:", bg = "white")
+        l1 = Label(self.frame2, text = "First name:", bg = "white")
         l1.grid(row=0, column=0,padx=10, pady=5, sticky=W)
-        l2 = Label(frame2, text = "Age:",bg = "White")
+        l2 = Label(self.frame2, text = "Age:",bg = "White")
         l2.grid(row=1, column=0, pady = 10, padx=10, sticky=W)
-        l3 = Label(frame2, text = "Do you have a mobile phone?", bg = "White")
+        l3 = Label(self.frame2, text = "Do you have a mobile phone?", bg = "White")
         l3.grid(row=2, column=0, padx=10, pady=10)
 
-        e1 = Entry(frame2)
+        e1 = Entry(self.frame2)
         e1.grid(row=0, column=1, sticky=W)
-        e2 = Entry(frame2)
+        e2 = Entry(self.frame2)
         e2.grid(row = 1, column=1, sticky=W)
 
         self.radio_var = StringVar()
         self.radio_var.set(self.data.radios)
 
         for radio in self.data.radios:
-            Radiobutton(frame2, text = radio, 
+            Radiobutton(self.frame2, text = radio, 
                         variable=self.radio_var, 
                         value=radio, bg = "White",
                         command=self.phone).grid(column=1, sticky=W)
 
-        button2 = Button(frame2, text = "Enter Data", bg = "white")
+        button2 = Button(self.frame2, text = "Enter Data", bg = "white")
         button2.grid(row=5, padx=30)
 
     
     def phone(self):
         pass
 
+    def frame_3(self):
+        self.frame2.grid_forget()
+        
+        self.data_label.configure(text = "Displaying Person Data")
+        frame3 = Frame(self.main_frame,highlightbackground="black", highlightthickness=1, 
+                                    height=200, 
+                                    width=300, bg = "White")
+        frame3.grid(row=1, column=0)
+        frame3.grid_propagate(0)
+
+        name_labl = Label(frame3, text = "First name: ", bg = "white")
+        name_labl.grid(row=0, column=0, padx=20,pady=4)
+        user_name = Label(frame3, text = "Not entered yet", bg = "White")
+        user_name.grid(row = 0, column=1, padx=30)
+
+        age_labl = Label(frame3, text = "Age: ", bg = "white")
+        age_labl.grid(row=1, column=0, padx=20,pady=4)
+        user_age = Label(frame3, text = "Not entered yet", bg = "White")
+        user_age.grid(row = 1, column=1, padx=30)
+
+
+
 
 class UI:
     def __init__(self):
         self.radios = ["No","Yes"]
+        self.name = []
+        self.age= []
+    
+    def has_phone(self):
+        pass
+    
 
 def main():
     root = Tk()
