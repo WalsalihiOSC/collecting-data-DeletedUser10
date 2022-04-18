1bae75dbac16fa8673842f03902027c4ecd12f82
Ayush Kumar <90948894+DeletedUser10@users.noreply.github.com>
2022-04-18 19:52:57 +1200

    Uses a loop to display data

--- Data collection task.py
+++ Data collection task.py
@@ -1,9 +1,19 @@
+
 from tkinter import*
 from tkinter import messagebox
 """Data collection task"""
+
+class Variables:
+    def __init__(self, name, age, phone):
+        self.name = name
+        self.age = age
+        self.phone = phone
+
+
 class Interface:
     def __init__(self, parent):
         self.data = UI()
+        
         self.main_frame = Frame(parent)
         self.main_frame.grid()
         
@@ -19,7 +29,7 @@ class Interface:
         self.data_label.grid(row=0, column=0, padx=10)
         ghost_Label = Label(self.frame1, text = "               ", bg = "Pink").grid(column=2, row=0)
 
-        self.button1 = Button(self.frame1, text = "Show All", command=self.frame_3)
+        self.button1 = Button(self.frame1, text = "Show All", command=self.display_data)
         self.button1.grid(column=3, row=0, pady=10,sticky=NSEW)
 
         self.frame2 = Frame(self.main_frame, highlightbackground="black", highlightthickness=1, 
@@ -40,6 +50,7 @@ class Interface:
         self.e2 = Entry(self.frame2)
         self.e2.grid(row = 1, column=1, sticky=W)
 
+        """ Radio buttons """
         self.radio_var = StringVar()
         self.radio_var.set(self.data.radios)
 
@@ -48,86 +59,73 @@ class Interface:
                         variable=self.radio_var, 
                         value=radio, bg = "White").grid(column=1, sticky=W)
 
-        self.button2 = Button(self.frame2, text = "Enter Data", bg = "white", command=self.check)
+        self.button2 = Button(self.frame2, text = "Enter Data", bg = "white", command=self.enter_data)
         self.button2.grid(row=5, padx=30)
 
 
-    """Creates frame 3"""
-    def frame_3(self):
-        self.frame2.grid_forget()
-        
-        self.data_label.configure(text = "Displaying Person Data")
-        self.frame3 = Frame(self.main_frame,highlightbackground="black", highlightthickness=1, 
-                                    height=200, 
-                                    width=300, bg = "White")
-        self.frame3.grid(row=1, column=0)
-        self.frame3.grid_propagate(0)
 
-        self.name_labl = Label(self.frame3, text = "First name: ", bg = "white")
-        self.name_labl.grid(row=0, column=0, padx=20,pady=4)
-        self.user_name = Label(self.frame3, text = "Not entered yet", bg = "White")
-        self.user_name.grid(row = 0, column=1, padx=30)
-
-        self.age_labl = Label(self.frame3, text = "Age: ", bg = "white")
-        self.age_labl.grid(row=1, column=0, padx=20,pady=4)
-        self.user_age = Label(self.frame3, text = "Not entered yet", bg = "White")
-        self.user_age.grid(row = 1, column=1, padx=30)
-
-        self.final_labl = Label(self.frame3, text = "", bg = "white")
-        self.final_labl.grid(row=2, column=1)
-
-        self.back_bttn = Button(self.frame3, text = "Add person ", bg = "White", command=self.back)
-        self.back_bttn.grid(row=3, column=0)
-
-        self.display_data()
     
-    def back(self):
-        self.frame3.grid_forget()
-        self.frame2.grid()
-
     def enter_data(self):
-        self.data.name.append(self.e1.get())
-        self.data.age.append(self.e2.get())
-        
-        "Radio_button"
-        if self.data.has_phone(self.radio_var.get()):
-            self.data.phones.append("doesn't have a mobile phone")
-        else:
-            self.data.phones.append("has a mobile phone")
+        self.data.people_data.append(Variables(self.e1.get(), self.e2.get(), self.radio_var.get()))
 
         self.e1.delete(0,END)
         self.e2.delete(0,END)
         self.radio_var.set(None)
 
-    def check(self):
-        self.err_check = 0
-        
-        if len(self.e1.get())== 0:
-            messagebox.showerror("name","name is required")
-            self.err_check = 1
-        elif len(self.e2.get()) == 0:
-            messagebox.showerror("age", "age is required")
-        else:
-            if self.err_check == 0:
-                self.enter_data()
 
+    """Creates frame 3"""
     def display_data(self):
-        self.user_name.configure(text = self.data.name,bg = "white")
-        self.user_age.configure(text = self.data.age,bg = "white")
-        self.final_labl.configure(text = f"{str(*self.data.name)} {str(*self.data.phones)}")
+        self.frame2.grid_forget()
+        self.data_label.configure(text = "Displaying Person Data")
+        self.frame3 = Frame(self.main_frame,highlightbackground="black", highlightthickness=1, 
+                                        height=200, 
+                                        width=300, bg = "White")
+        self.frame3.grid(row=1, column=0)
 
+
+        
+        for data in self.data.people_data:
+    
+            self.framel = Frame(self.frame3, bg = "white" ,highlightbackground="black", highlightthickness=1, width=300)
+            self.framel.grid(column=0)
+            
+            self.name_labl = Label(self.framel, text = "First name: ", bg = "white")
+            self.age_labl = Label(self.framel, text = "Age: ", bg = "white")
+
+
+            self.user_name = Label(self.framel, text = data.name, bg = "White")
+            self.user_age = Label(self.framel, text = data.age, bg = "White")
+            self.final_labl = Label(self.framel, text = f"{data.name} aged {data.age} \n has phone: {data.phone}", bg = "white")
+            
+            self.name_labl.grid(row=1, column=0, padx=10,pady=4)
+            self.age_labl.grid(row=2, column=0, padx=10,pady=4)
+
+            self.user_name.grid(row = 1, column=1, padx=20)
+            self.user_age.grid(row = 2, column=1, padx=20)
+            self.final_labl.grid(row=5, column=0, padx=10, pady=30, sticky=E)
+    
+        
+        self.back_bttn = Button(self.frame3, text = "Add person ", bg = "White", command=self.back)
+        self.back_bttn.grid(row=3, column=0)
+        self.button1.grid_forget()
+
+
+
+
+    
+    def back(self):
+        self.frame3.grid_forget()
+        self.button1.grid(column=3, row=0, pady=10,sticky=NSEW)
+        self.frame2.grid(row=1)
+        
+  
 
 """Ui class"""
 class UI:
-    def __init__(self):
-        self.radios = ["No","Yes"]
-        self.name = []
-        self.age= []
-        self.phones = []
-    
-    def has_phone(self, answer):
-       return answer == self.radios[0]
-    
+    def __init__(self, people_data = []):
+        self.radios = ["No", "Yes"]
+        self.people_data = people_data
+        
 
 def main():
     root = Tk()
